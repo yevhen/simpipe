2ef930fd1ea9e8188e272a4c73ad25e444f2f6f7
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2023-08-23 11:20:19 +0300

    Tidy up

--- BatchBlock.go
+++ BatchBlock.go
@@ -20,31 +20,39 @@ func (block *BatchBlock[T]) Run() {
 			select {
 			case item, ok := <-block.Input:
 				if !ok {
-					if len(block.buffer) > 0 {
-						block.flushBuffer()
-						return
-					}
-				}
-				block.buffer = append(block.buffer, item)
-				if len(block.buffer) == block.BatchSize {
 					block.flushBuffer()
-					block.restartTimer()
-					block.batchFlushed = true
+					return
 				}
+				block.flushBySize(item)
 			case <-block.timer.C:
-				if block.batchFlushed {
-					block.batchFlushed = false
-					continue
-				}
-				if len(block.buffer) > 0 {
-					block.flushBuffer()
-				}
+				block.flushByTimer()
 			}
 		}
 	}()
 }
 
+func (block *BatchBlock[T]) flushBySize(item T) {
+	block.buffer = append(block.buffer, item)
+	if len(block.buffer) == block.BatchSize {
+		block.flushBuffer()
+		block.restartTimer()
+		block.batchFlushed = true
+	}
+}
+
+func (block *BatchBlock[T]) flushByTimer() {
+	if block.batchFlushed {
+		block.batchFlushed = false
+		return
+	}
+	block.flushBuffer()
+}
+
 func (block *BatchBlock[T]) flushBuffer() {
+	if len(block.buffer) == 0 {
+		return
+	}
+
 	batch := make([]T, len(block.buffer))
 	copy(batch, block.buffer)
 
