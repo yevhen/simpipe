333600848adb67eaf713943f128b424d54b17530
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2023-08-28 20:06:45 +0300

    Finish with action and batch pipes

--- Pipe_test.go
+++ Pipe_test.go
@@ -6,31 +6,31 @@ import (
 	"time"
 )
 
-type PipeSender[T any] interface {
+type Pipe[T any] interface {
 	Send(item T)
 }
 
-type Pipe[T any] struct {
-	Receiver func(item T)
-	Filter   func(item T) bool
-	Next     func(item T) PipeSender[T]
+type PipeLink[T any] struct {
+	receiver func(item T)
+	filter   func(item T) bool
+	next     func(item T) Pipe[T]
 }
 
-func (pipe *Pipe[T]) Send(item T) {
-	if pipe.Filter(item) {
-		pipe.Receiver(item)
+func (p *PipeLink[T]) Send(item T) {
+	if p.filter(item) {
+		p.receiver(item)
 		return
 	}
 
-	pipe.sendNext(item)
+	p.sendNext(item)
 }
 
-func (pipe *Pipe[T]) sendNext(item T) {
-	if pipe.Next == nil {
+func (p *PipeLink[T]) sendNext(item T) {
+	if p.next == nil {
 		return
 	}
 
-	next := pipe.Next(item)
+	next := p.next(item)
 	if next != nil {
 		next.Send(item)
 	}
@@ -44,8 +44,8 @@ func TestSendsItem(t *testing.T) {
 
 	const s = "foo"
 
-	pipe := createPipe(action)
-	pipe.Send(s)
+	p := createPipe(action)
+	p.Send(s)
 
 	assert.Equal(t, sent, s)
 }
@@ -61,25 +61,25 @@ func TestDoesNotPassFilteredItemToReceiver(t *testing.T) {
 		return item != "foo"
 	}
 
-	pipe := createFilteredPipe(action, filter)
-	pipe.Send(s)
+	p := createFilteredPipe(action, filter)
+	p.Send(s)
 
 	assert.Empty(t, sent)
 }
 
-type PipeSenderMock[T any] struct {
+type PipeMock[T any] struct {
 	ReceivedItem T
 }
 
-func (p *PipeSenderMock[T]) Send(item T) {
+func (p *PipeMock[T]) Send(item T) {
 	p.ReceivedItem = item
 }
 
 func TestPassesFilteredItemToNextPipe(t *testing.T) {
-	nextPipe := new(PipeSenderMock[string])
+	nextPipe := new(PipeMock[string])
 
-	next := func(item string) PipeSender[string] {
-		var p PipeSender[string] = nextPipe
+	next := func(item string) Pipe[string] {
+		var p Pipe[string] = nextPipe
 		return p
 	}
 
@@ -95,7 +95,7 @@ func TestPassesFilteredItemToNextPipe(t *testing.T) {
 
 func TestPassesFilteredItemToNextAndNextReturnsNil(t *testing.T) {
 	var received string
-	next := func(item string) PipeSender[string] {
+	next := func(item string) Pipe[string] {
 		received = item
 		return nil
 	}
@@ -103,74 +103,187 @@ func TestPassesFilteredItemToNextAndNextReturnsNil(t *testing.T) {
 	filter := func(item string) bool {
 		return false
 	}
+
 	pipe := createFilteredPipeWithNext(filter, next)
 	pipe.Send("foo")
 
 	assert.Equal(t, "foo", received)
 }
 
-func TestIntegration(t *testing.T) {
-	input := make(chan string)
-
-	pipe := Pipe[string]{
-		Receiver: func(item string) {
-			input <- item
-		},
-		Filter: func(item string) bool {
-			return true
-		},
-		Next: nil,
-	}
-
-	var sent string
+func TestActionPipeBlockIntegration(t *testing.T) {
+	sent := make(chan string)
 	action := func(item string) {
-		sent = item
+		sent <- item
 	}
 
-	block := ActionBlock[string]{
-		Input:       input,
-		Parallelism: 1,
-		Action:      action,
-		Done:        pipe.sendNext,
+	filter := func(item string) bool {
+		return item == "foo"
 	}
-	block.Run()
+
+	var nextReceived = make([]string, 0)
+	next := func(item string) Pipe[string] {
+		nextReceived = append(nextReceived, item)
+		return nil
+	}
+
+	pipe := CreateActionPipe(2, 2, action, filter, next)
+	pipe.Run()
 
 	pipe.Send("foo")
-	time.Sleep(100 * time.Millisecond)
+	pipe.Send("bar")
+	pipe.Close()
 
-	assert.Equal(t, "foo", sent)
+	assert.Equal(t, "foo", <-sent)
+	assert.ElementsMatch(t, []string{"foo", "bar"}, nextReceived)
 }
 
-func createPipe[T any](action func(item T)) *Pipe[T] {
+func TestBatchPipeBlockIntegration(t *testing.T) {
+	sent := make(chan []string)
+	action := func(items []string) {
+		sent <- items
+	}
+
+	filter := func(item string) bool {
+		return item == "foo"
+	}
+
+	var nextReceived = make([]string, 0)
+	next := func(item string) Pipe[string] {
+		nextReceived = append(nextReceived, item)
+		return nil
+	}
+
+	pipe := CreateBatchActionPipe(4, 2, 2, action, filter, next)
+	pipe.Run()
+
+	pipe.Send("foo")
+	pipe.Send("bar")
+	pipe.Send("foo")
+	pipe.Close()
+
+	assert.ElementsMatch(t, []string{"foo", "foo"}, <-sent)
+	assert.ElementsMatch(t, []string{"foo", "foo", "bar"}, nextReceived)
+}
+
+type ActionPipe[T any] struct {
+	in    chan T
+	link  *PipeLink[T]
+	block *ActionBlock[T]
+}
+
+func (p *ActionPipe[T]) Run() {
+	p.block.Run()
+}
+
+func (p *ActionPipe[T]) Send(item T) {
+	p.link.Send(item)
+}
+
+func (p *ActionPipe[T]) Close() {
+	close(p.in)
+}
+
+func CreateActionPipe[T any](
+	capacity int,
+	parallelism int,
+	action func(item T),
+	filter func(item T) bool,
+	next func(item T) Pipe[T],
+) *ActionPipe[T] {
+	input := make(chan T, capacity)
+
+	pipe := &PipeLink[T]{
+		receiver: func(item T) {
+			input <- item
+		},
+		filter: filter,
+		next:   next,
+	}
+
+	block := CreateActionBlock(input, pipe.sendNext, parallelism, action)
+
+	return &ActionPipe[T]{
+		in:    input,
+		link:  pipe,
+		block: block,
+	}
+}
+
+type BatchActionPipe[T any] struct {
+	in    chan T
+	link  *PipeLink[T]
+	block *BatchActionBlock[T]
+}
+
+func (p *BatchActionPipe[T]) Run() {
+	p.block.Run()
+}
+
+func (p *BatchActionPipe[T]) Send(item T) {
+	p.link.Send(item)
+}
+
+func (p *BatchActionPipe[T]) Close() {
+	close(p.in)
+}
+
+func CreateBatchActionPipe[T any](
+	capacity int,
+	parallelism int,
+	batchSize int,
+	action func(items []T),
+	filter func(item T) bool,
+	next func(item T) Pipe[T],
+) *BatchActionPipe[T] {
+	input := make(chan T, capacity)
+
+	pipe := &PipeLink[T]{
+		receiver: func(item T) {
+			input <- item
+		},
+		filter: filter,
+		next:   next,
+	}
+
+	block := CreateBatchActionBlock(input, pipe.sendNext, batchSize, time.Hour, parallelism, action)
+
+	return &BatchActionPipe[T]{
+		in:    input,
+		link:  pipe,
+		block: block,
+	}
+}
+
+func createPipe[T any](action func(item T)) *PipeLink[T] {
 	filter := func(item T) bool {
 		return true
 	}
-	next := func(item T) PipeSender[T] {
+	next := func(item T) Pipe[T] {
 		return nil
 	}
 	return createCompletePipe(action, filter, next)
 }
 
-func createFilteredPipe[T any](receiver func(item T), filter func(item T) bool) *Pipe[T] {
-	next := func(item T) PipeSender[T] {
+func createFilteredPipe[T any](receiver func(item T), filter func(item T) bool) *PipeLink[T] {
+	next := func(item T) Pipe[T] {
 		return nil
 	}
 	return createCompletePipe(receiver, filter, next)
 }
 
-func createFilteredPipeWithNext[T any](filter func(item T) bool, next func(item T) PipeSender[T]) *Pipe[T] {
+func createFilteredPipeWithNext[T any](filter func(item T) bool, next func(item T) Pipe[T]) *PipeLink[T] {
 	action := func(item T) {
 	}
 	p := createPipe(action)
-	p.Filter = filter
-	p.Next = next
+	p.filter = filter
+	p.next = next
 	return p
 }
 
-func createCompletePipe[T any](receiver func(item T), filter func(item T) bool, next func(item T) PipeSender[T]) *Pipe[T] {
-	return &Pipe[T]{
-		Filter:   filter,
-		Receiver: receiver,
-		Next:     next,
+func createCompletePipe[T any](receiver func(item T), filter func(item T) bool, next func(item T) Pipe[T]) *PipeLink[T] {
+	return &PipeLink[T]{
+		filter:   filter,
+		receiver: receiver,
+		next:     next,
 	}
 }
